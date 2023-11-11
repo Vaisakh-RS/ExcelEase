1e51aad39f9c535b48e9b1d1a2b0ac63c1508e5c
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-11-11 19:56:56 +0530

    Implement file check

--- src/components/CsvFileUpload.jsx
+++ src/components/CsvFileUpload.jsx
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import Papa from 'papaparse';
+import * as XLSX from 'xlsx';
 
 // eslint-disable-next-line react/prop-types
 const CsvFileUpload = ({ onUpload }) => {
@@ -8,16 +9,35 @@ const CsvFileUpload = ({ onUpload }) => {
         (acceptedFiles) => {
             const file = acceptedFiles[0];
 
-            Papa.parse(file, {
-                complete: (result) => {
-                    const data = result.data;
-                    onUpload(data);
-                },
-                header: true,
-            });
+            if(file.type=='text/csv')
+            {
+                Papa.parse(file, {
+                    complete: (result) => {
+                        const data = result.data;
+                        onUpload(data);
+                    },
+                    header: true,
+                });
+          
+            }else if(
+                file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
+                file.type === 'application/vnd.ms-excel'
+            ){
+                const arrayBuffer =  file.arrayBuffer();
+                const workbook = XLSX.read(arrayBuffer, { type: 'array' });
+                const sheetName = workbook.SheetNames[0];
+                const sheet = workbook.Sheets[sheetName];
+                const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+                onUpload(data);
+            }
+            else{
+                alert('Unsupported file type. Please upload a CSV or XLSX file.');
+            }
         },
-        [onUpload],
+            [onUpload]
     );
+
+          
 
     const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
