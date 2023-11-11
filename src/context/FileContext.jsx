721908ce2b833c35d83747d4c8ec08ca993bb6c8
Ijas Iqbal <ijasiqbal4@gmail.com>
2023-11-11 17:01:00 +0530

    setup context

--- src/context/FileContext.jsx
+++ src/context/FileContext.jsx
@@ -3,34 +3,34 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 const FileContext = createContext();
 
 export const FileProvider = ({ children }) => {
-  const handleStorageChange = () => {
-    const storedFile = localStorage.getItem('file');
-    setUploadedFile(storedFile ? storedFile : null);
-  };
-
-  const [uploadedFile, setUploadedFile] = useState(() => {
-    // Use a function as the initial state
-    const storedFile = localStorage.getItem('file');
-    return storedFile ? storedFile : null;
-  });
-
-  useEffect(() => {
-    window.addEventListener('storage', handleStorageChange);
-
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
+    const handleStorageChange = () => {
+        const storedFile = localStorage.getItem('file');
+        setUploadedFile(storedFile ? storedFile : null);
     };
-  }, []);
 
-  return (
-    <FileContext.Provider value={{ uploadedFile, setUploadedFile }}>
-      {children}
-    </FileContext.Provider>
-  );
+    const [uploadedFile, setUploadedFile] = useState(() => {
+        // Use a function as the initial state
+        const storedFile = localStorage.getItem('file');
+        return storedFile ? storedFile : null;
+    });
+
+    useEffect(() => {
+        window.addEventListener('storage', handleStorageChange);
+
+        return () => {
+            window.removeEventListener('storage', handleStorageChange);
+        };
+    }, []);
+
+    return (
+        <FileContext.Provider value={{ uploadedFile, setUploadedFile }}>
+            {children}
+        </FileContext.Provider>
+    );
 };
 
 export const useFileContext = () => {
-  return useContext(FileContext);
+    return useContext(FileContext);
 };
 
 export { FileContext };
--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -4,19 +4,18 @@ import { useFileContext } from '../context/FileContext.jsx';
 const Upload = () => {
     const { uploadedFile, setUploadedFile } = useFileContext();
 
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        localStorage.setItem('file', file);
+        setUploadedFile(file);
+    };
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    localStorage.setItem('file', file);
-    setUploadedFile(file)
-  };
-
-  return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      {uploadedFile ? <p>Uploaded file:{uploadedFile.name}</p>:null}
-    </div>
-  );
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            {uploadedFile ? <p>Uploaded file:{uploadedFile.name}</p> : null}
+        </div>
+    );
 };
 
 export default Upload;
