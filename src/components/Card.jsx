e96ed73677b6250c885127e6078a7c6e622795d1
Abhijith Kp <abhijithkp773@gmail.com>
2023-11-11 15:17:25 +0530

    removed lint

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,14 @@
-import React from "react";
-
-
 const Card = ({ data }) => {
     return (
-      <div>
-        <h3>Card</h3>
-        <ul>
-          {data.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </div>
+        <div>
+            <h3>Card</h3>
+            <ul>
+                {data.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
     );
-  };
-  
-  export default Card;
+};
+
+export default Card;
