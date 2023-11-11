68cede81f6f824a20f4cd3c3dbf1bb7c3e16cf85
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-11-11 18:33:16 +0530

    Add styling to the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,27 @@
-// eslint-disable-next-line react/prop-types
 const Card = ({ data }) => {
     return (
-        <div>
+        <div className="bg-white p-4 rounded-lg shadow-md">
             <div>
-                <p>---------------------</p>
+                <p className="border-b-2 border-gray-300 mb-4 pb-2">---------------------</p>
 
-                <h5>Card </h5>
-                <p>============</p>
+                <h5 className="text-xl font-semibold mb-2">Card</h5>
+                <p className="text-lg font-semibold mb-4">============</p>
 
                 {Object.entries(data).map(([key, value]) => (
-                    <div key={key}>
-                        <br />
-                        <h2>{key}</h2>
-                        <input type="text" value={value} />
+                    <div key={key} className="mb-4 flex items-center">
+                        <div className="w-1/6">
+                            <h2 className="text-lg font-semibold mb-1">{key}</h2>
+                        </div>
+                        <div className="w-5/6">
+                            <input
+                                type="text"
+                                value={value}
+                                className="w-1/4 p-2 border border-gray-300 rounded"
+                            />
+                        </div>
                     </div>
                 ))}
-                <p>---------------------</p>
+                <p className="border-t-2 border-gray-300 pt-2">---------------------</p>
             </div>
         </div>
     );
