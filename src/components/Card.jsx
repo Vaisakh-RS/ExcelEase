a19c956ecdc5e4758c2259a2c472bee897958d2f
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-11-11 15:08:10 +0530

    Create card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Card = ({ data }) => {
+    return (
+      <div>
+        <h3>Card</h3>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Card;
---
+++ src/components/CardDataRender.jsx
@@ -0,0 +1,28 @@
+
+import { useState } from 'react';
+import Card from './Card'; 
+import ExcelReader from './ExcelReader';
+
+const App = () => {
+  const [excelData, setExcelData] = useState(null);
+
+  const handleFileUpload = (data) => {
+    setExcelData(data);
+  };
+
+  return (
+    <div>
+      <ExcelReader onFileUpload={handleFileUpload} />
+      {excelData && (
+        <div>
+          <h2>Data from Excel:</h2>
+          {excelData.map((row, index) => (
+            <Card key={index} data={row} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
