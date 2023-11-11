7f96724f56d81b6e9bec2b6671fa0b8df3dbfcac
Ijas Iqbal <ijasiqbal4@gmail.com>
2023-11-11 17:00:29 +0530

    setup context

---
+++ src/pages/Upload.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { useFileContext } from '../context/FileContext.jsx';
+
+const Upload = () => {
+    const { uploadedFile, setUploadedFile } = useFileContext();
+
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    localStorage.setItem('file', file);
+    setUploadedFile(file)
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      {uploadedFile ? <p>Uploaded file:{uploadedFile.name}</p>:null}
+    </div>
+  );
+};
+
+export default Upload;
