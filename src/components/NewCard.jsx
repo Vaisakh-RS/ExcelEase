3e1b5a14373f7b1b91f96c561018e62ddad0c885
Abhijith Kp <abhijithkp773@gmail.com>
2023-11-12 10:41:37 +0530

    new card styles

--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import '../styles/addNewCard.css';
 
 const NewCard = ({ data, onClose, updateCsvData }) => {
     const initialFormState = Object.fromEntries(
@@ -23,46 +24,49 @@ const NewCard = ({ data, onClose, updateCsvData }) => {
     };
 
     return (
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-14 rounded-lg shadow-md border-2">
-            <div>
-                <h5 className="text-2xl font-semibold mb-10">Add Data</h5>
+        <>
+            <div className="blur"></div>
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-14 rounded-lg shadow-md border-2 newData">
+                <div>
+                    <h5 className="text-2xl font-semibold mb-10">Add Data</h5>
 
-                {Object.entries(data).map(([key, value]) => (
-                    <div key={key} className="mb-4 flex items-center">
-                        <div className="w-3/6">
-                            <h2 className="text-lg font-semibold mb-1">
-                                {key}
-                            </h2>
+                    {Object.entries(data).map(([key, value]) => (
+                        <div key={key} className="mb-4 flex items-between">
+                            <div className="w-3/6">
+                                <h2 className="text-lg font-semibold mb-1">
+                                    {key}
+                                </h2>
+                            </div>
+                            <div className="w-3/6">
+                                <input
+                                    type="text"
+                                    value={formData[key]}
+                                    name={key}
+                                    onChange={(e) =>
+                                        handleInputChange(key, e.target.value)
+                                    }
+                                    className="p-2 border border-gray-300 rounded"
+                                />
+                            </div>
                         </div>
-                        <div className="w-3/6">
-                            <input
-                                type="text"
-                                value={formData[key]}
-                                name={key}
-                                onChange={(e) =>
-                                    handleInputChange(key, e.target.value)
-                                }
-                                className="p-2 border border-gray-300 rounded"
-                            />
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
+                <div className="flex justify-between">
+                    <button
+                        className="border border-gray-500 hover:border-green-500 px-4 py-2 rounded transition duration-300 ease-in-out"
+                        onClick={handleAddCard}
+                    >
+                        Add Card
+                    </button>
+                    <button
+                        className="border border-gray-500 hover:border-red-500 px-4 py-2 rounded transition duration-300 ease-in-out"
+                        onClick={onClose}
+                    >
+                        Close
+                    </button>
+                </div>
             </div>
-            <div className="flex justify-between">
-                <button
-                    className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-                    onClick={handleAddCard}
-                >
-                    Add Card
-                </button>
-                <button
-                    className="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-700"
-                    onClick={onClose}
-                >
-                    Close
-                </button>
-            </div>
-        </div>
+        </>
     );
 };
 
