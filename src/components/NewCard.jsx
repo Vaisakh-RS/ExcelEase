233c657a3434955e54c394d49ce52bcf7a3a1c48
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-11-14 15:29:02 +0530

    Add responsivness to the add new data card

--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -27,25 +27,23 @@ const NewCard = ({ data, onClose, updateCsvData }) => {
     return (
         <>
             <div className="blur"></div>
-            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-14 rounded-lg shadow-md border-2 newData">
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-14 rounded-lg shadow-md border-2 newData w-full sm:w-2/3 md:w-1/2">
                 <div>
                     <h5 className="text-2xl font-semibold mb-10">Add Data</h5>
-
+    
                     {Object.entries(data).map(([key, value]) => (
-                        <div key={key} className="mb-4 flex items-between">
-                            <div className="w-3/6">
+                        <div key={key} className="mb-4 flex flex-col sm:flex-row items-between">
+                            <div className="w-full sm:w-3/6 mb-2 sm:mb-0">
                                 <h2 className="text-lg font-semibold mb-1">
                                     {key}
                                 </h2>
                             </div>
-                            <div className="w-3/6">
+                            <div className="w-full sm:w-3/6">
                                 <input
                                     type="text"
                                     value={formData[key]}
                                     name={key}
-                                    onChange={(e) =>
-                                        handleInputChange(key, e.target.value)
-                                    }
+                                    onChange={(e) => handleInputChange(key, e.target.value)}
                                     className="p-2 border border-gray-300 rounded"
                                 />
                             </div>
@@ -69,6 +67,7 @@ const NewCard = ({ data, onClose, updateCsvData }) => {
             </div>
         </>
     );
+    
 };
 
 export default NewCard;
